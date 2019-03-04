dac1159f9b2ee9e2724137cbabf016af66b9958a
samah97 <38073406+samah97@users.noreply.github.com>
2019-03-04 23:55:42 +0200

    ss

---
+++ Model/class/mysql/ext/MedicationIntakeMySqlExtDAO.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Class that operate on table 'medication_intake'. Database Mysql.
+ *
+ * @author: http://phpdao.com
+ * @date: 2019-03-04 22:53
+ */
+class MedicationIntakeMySqlExtDAO extends MedicationIntakeMySqlDAO{
+
+	
+}
+?>
