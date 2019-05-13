9aba57e89e181d737a02f077811ce8df11f842e3
Ehab <Ehab@DESKTOP-NIL0F9F.lan>
2019-05-13 22:00:28 +0300

    ee

---
+++ pages/brands.php
@@ -0,0 +1,23 @@
+<?php
+include 'menu.php';
+//samah
+?>
+
+
+<div class="container-fluid">
+  <h1>Add Brands</h1>
+  <div class="row">
+    <form>
+    <div class="row">
+      <div class="col-md-4"><label for="brandFound" class="col-md-4">Brands Found:</label></div>
+      <div class="col-md-4"><textarea name="brandFound" id="brandFound" class="form-control"></textarea></div>
+    </div>
+    <div class="row form-group">
+      <div class="col-md-4"><label for="brand" class="col-md-4">Brand:</label></div>
+      <div class="col-md-4"><input type="text" name="brand" id="brand" class="form-control"></input></div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
